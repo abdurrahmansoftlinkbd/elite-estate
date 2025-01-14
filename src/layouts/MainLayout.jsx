8777ca84f1159f00a911d4a4bf8e7b579d841092
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-14 16:30:10 +0600

    added raw navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
 
 const MainLayout = () => {
   return (
     <>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
       <Outlet></Outlet>
     </>
   );
