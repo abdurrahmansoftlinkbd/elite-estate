aa149c72262491b20d5880f7808f89f5b4128820
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-16 08:26:37 +0600

    axiosSecure

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import AuthProvider from "./providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
