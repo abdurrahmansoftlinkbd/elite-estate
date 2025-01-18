0ab52b232f17119fbe30e6e07ce43db6c9c75459
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-18 11:14:44 +0600

    added loading in login button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import AuthContext from "../providers/AuthContext";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const Login = () => {
   const { userLogin, setUser, handleGoogleSignIn } = useContext(AuthContext);
-  const location = useLocation();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
 
-    userLogin(email, password)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-        form.reset();
-        navigate(location?.state ? location.state : "/");
-        toast.success("Welcome back!");
-      })
-      .catch((error) => {
-        toast.error(`${error.code}`);
-      });
+    try {
+      const result = await userLogin(email, password);
+      const user = result.user;
+      setUser(user);
+      form.reset();
+      navigate("/");
+      toast.success("Welcome back!");
+    } catch (error) {
+      toast.error(error?.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -60,13 +62,24 @@ const Login = () => {
             />
           </div>
           <div className="form-control mt-6">
-            <button className="btn bg-default text-white hover:bg-light hover:border-light ">
-              Login
+            <button
+              className="btn bg-default text-white hover:bg-light hover:border-light"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <span className="loading loading-spinner loading-xs"></span>
+                </>
+              ) : (
+                "Login"
+              )}
             </button>
             <div className="divider">OR</div>
             <button
               onClick={handleGoogleSignIn}
               className=" btn bg-base-200 hover:bg-base-100"
+              disabled={loading}
             >
               <FcGoogle className="text-2xl" />
               Sign in with Google
