4366aea2a466ec3cc1ea875fe2376b7702aa7167
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-18 19:27:00 +0600

    removed private route from allProperties page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -46,11 +46,7 @@ const router = createBrowserRouter([
       },
       {
         path: "allProperties",
-        element: (
-          <PrivateRoute>
-            <AllPropertiesPage></AllPropertiesPage>
-          </PrivateRoute>
-        ),
+        element: <AllPropertiesPage></AllPropertiesPage>,
       },
       {
         path: "login",
